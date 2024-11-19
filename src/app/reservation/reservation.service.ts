ffe007bbe75e37b8c5419ab2c28e27758aa5b370
Samantha Muellner <muellner@metsci.com>
2024-11-19 11:53:34 -0500

    Add basics

---
+++ src/app/reservation/reservation.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+
+@Injectable({
+  providedIn: 'root',
+}) 
+export class ReservationService { 
+  private reservations: Reservation[] = [];
+
+  constructor() {
+    const savedReservations = localStorage.getItem('reservations');
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
+  // CRUD
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find((res) => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    reservation.id = Date.now().toString();
+    this.reservations.push(reservation);
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  deleteReservation(id: string): void {
+    const index = this.reservations.findIndex((res) => res.id === id);
+    if (index !== undefined) {
+      this.reservations.splice(index, 1);
+    }
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  deleteAllReservations(): void {
+    this.reservations = [];
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+
+  updateReservation(id: string, updatedReservation: Reservation): void {
+    const index = this.reservations.findIndex((res) => res.id === id);
+    updatedReservation.id = id;
+
+    this.reservations[index] = updatedReservation;
+    localStorage.setItem('reservations', JSON.stringify(this.reservations));
+  }
+}
+ 
