53695bedec5002aefdd8cac277a13a64e43a9621
Samantha <samantha.h.hamill@gmail.com>
2024-12-03 20:53:02 -0500

    Update and clean sorting elements

--- src/app/box-list/box-list.component.ts
+++ src/app/box-list/box-list.component.ts
@@ -9,6 +9,16 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+
+export enum SortOptions {
+  NAME_LOW_HIGH = "Name: Low to High",
+  NAME_HIGH_LOW = "Name: High to Low",
+  SIZE_LOW_HIGH = "Size: Low to High",
+  SIZE_HIGH_LOW = "Size: High to Low",
+  ROOM_LOW_HIGH = "Room: Low to High",
+  ROOM_HIGH_LOW = "Room: High to Low",
+}
+
 @Component( {
   selector: 'app-box-list',
   templateUrl: './box-list.component.html',
@@ -29,7 +39,8 @@ import { RouterModule } from '@angular/router';
 export class BoxListComponent implements OnInit {
   boxes: Array<Box> = [];
   filteredBoxes: Array<Box> = [];
-  sortOrder: string = "";
+  sortOrder: SortOptions = SortOptions.NAME_LOW_HIGH;
+  sortOptions: Array<SortOptions> = Object.values(SortOptions);
 
   constructor( private boxService: BoxService ) {}
 
@@ -65,24 +76,31 @@ export class BoxListComponent implements OnInit {
     return description !== undefined ? `| ${description}` : "";
   }
 
-  sortProducts( sortValue: string ): void {
+  sortProducts( sortValue: SortOptions ): void {
     this.sortOrder = sortValue;
 
-    if ( this.sortOrder === "sizeLowHigh" )
+    switch( this.sortOrder )
     {
-      this.filteredBoxes.sort( ( a, b ) => boxSizeToNumber( a.size ) - boxSizeToNumber( b.size ) );
-    }
-    else if ( this.sortOrder === "sizeHighLow" )
-    {
-      this.filteredBoxes.sort( ( a, b ) => boxSizeToNumber( b.size ) - boxSizeToNumber( a.size ) );
-    }
-    else if ( this.sortOrder === "roomAZ" )
-    {
-      this.filteredBoxes.sort( ( a, b ) => a.associatedRoom.localeCompare( b.associatedRoom ) );
-    }
-    else if ( this.sortOrder === "roomZA" )
-    {
-      this.filteredBoxes.sort( ( a, b ) => b.associatedRoom.localeCompare( a.associatedRoom ) );
+      case SortOptions.SIZE_LOW_HIGH:
+        this.filteredBoxes.sort( ( a, b ) => boxSizeToNumber( a.size ) - boxSizeToNumber( b.size ) );
+        break;
+      case SortOptions.SIZE_HIGH_LOW:
+        this.filteredBoxes.sort( ( a, b ) => boxSizeToNumber( b.size ) - boxSizeToNumber( a.size ) );
+        break;
+      case SortOptions.ROOM_LOW_HIGH:
+        this.filteredBoxes.sort( ( a, b ) => a.associatedRoom.localeCompare( b.associatedRoom ) );
+        break;
+      case SortOptions.ROOM_HIGH_LOW:
+        this.filteredBoxes.sort( ( a, b ) => b.associatedRoom.localeCompare( a.associatedRoom ) );
+        break;
+      case SortOptions.NAME_HIGH_LOW:
+        this.filteredBoxes.sort( ( a, b ) => a.name.localeCompare( b.name ) );
+        break;
+      case SortOptions.NAME_LOW_HIGH:
+        this.filteredBoxes.sort( ( a, b ) => b.name.localeCompare( a.name ) );
+        break;
+      default: 
+        break;
     }
   }
 }
