bde8c8d5c451518f4335c93cc5330c023c7dceb7
Samantha <samantha.h.hamill@gmail.com>
2024-12-04 17:36:31 -0500

    Fix naming

--- src/app/components/countdown-timer/popout/countdown-timer-popout.component.ts
+++ src/app/components/countdown-timer/popout/countdown-timer-popout.component.ts
@@ -3,7 +3,6 @@ import { CountdownTimerComponent } from '../countdown-timer.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
 import { TimerService } from '../../../services/timer/timer.service';
 
 @Component( {
@@ -11,7 +10,6 @@ import { TimerService } from '../../../services/timer/timer.service';
   standalone: true,
   imports: [
     CountdownTimerComponent,
-    CommonModule,
     MatIconModule,
     MatButtonModule,
     MatMenuModule
@@ -20,8 +18,5 @@ import { TimerService } from '../../../services/timer/timer.service';
   styleUrl: "./countdown-timer-popout.component.scss"
 } )
 export class CountdownTimerPopoutComponent  {
-  constructor( protected timerService: TimerService )
-  {
-
-  }
+  constructor( protected timerService: TimerService ) { }
 }
